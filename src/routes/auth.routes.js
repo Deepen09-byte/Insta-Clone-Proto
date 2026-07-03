1c22fd8f3fa6d3f434dcb02604e726af59317398
Deepen09-byte <deepen2918@gmail.com>
2026-07-03 22:03:50 +0530

    Auth controller file added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,92 +2,12 @@ const express = require("express");
 const crypto = require("crypto");
 const userModel = require("../models/users.model");
 const jwt = require("jsonwebtoken");
+const authController = require("../controllers/auth.controllers")
 
 const authRouter = express.Router();
 
-authRouter.post("/register", async (req, res) => {
-  const { username, email, password, bio, profileImage } = req.body;
+authRouter.post("/register", authController.registerController);
 
-  const ifUserExists = await userModel.findOne({
-    $or: [{ username }, { email }],
-  });
-
-  if (ifUserExists) {
-    return res
-      .status(409)
-      .json({
-        message:
-          "User already exists" + ifUserExists.email == email
-            ? "Email already exists"
-            : "Username already exists",
-      });
-  }
-
-  const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-  const user = await userModel.create({
-    username,
-    email,
-    bio,
-    profileImage,
-    password: hash,
-  });
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-
-  res.cookie("token", token);
-
-  res
-    .status(201)
-    .json({
-      message: "User registered successfully",
-      user: {
-        username: user.username,
-        email: user.email,
-        bio: user.bio,
-        profileImage: user.profileImage,
-      },
-    });
-});
-
-authRouter.post("/login", async (req, res) => {
-  const { email, username, password } = req.body;
-
-  const user = await userModel.findOne({
-    $or: [{ email }, { username }],
-  });
-
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-  const isPasswordCorrect = hash === user.password;
-
-  if (!isPasswordCorrect) {
-    return res.status(401).json({ message: "Invalid credentials" });
-  }
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-
-  res.cookie("token", token);
-
-  res
-    .status(200)
-    .json({
-      message: "Login successful",
-      user: {
-        username: user.username,
-        email: user.email,
-        bio: user.bio,
-        profileImage: user.profileImage,
-      },
-    });
-});
+authRouter.post("/login", authController.loginController);
 
 module.exports = authRouter;
